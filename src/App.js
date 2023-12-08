93552e63b87ad398529e30b1f42e00fed07101f1
lefevretom60 <tomlefevre60@gmail.com>
2023-12-08 10:57:08 +0100

    continuite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
         <div class="login-box">
-  <h2>Login</h2>
-  <form>
-    <div class="user-box">
-      <input type="text" name="" required=""></input>
-      <label>Username</label>
-    </div>
-    <div class="user-box">
-      <input type="password" name="" required=""></input>
-      <label>Password</label>
-    </div>
-    
-  </form>
-</div>
+          <h2>Login</h2>
+          <form>
+            <div class="user-box">
+              <input type="text" name="" required=""></input>
+              <label>Username</label>
+            </div>
+            <div class="user-box">
+              <input type="password" name="" required=""></input>
+              <label>Password</label>
+            </div>
+          </form>
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import ComposantReact from './composant/ComposantReact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ComposantReact from "./composant/ComposantReact";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ComposantReact couleur={'yellow'} />
+    <ComposantReact couleur={"yellow"} />
   </React.StrictMode>
 );
 
